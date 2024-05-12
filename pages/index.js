28e968dc0d1bc6d6157bee2184877ad9cacd610a
Bill Wilder <codingoutloud@gmail.com>
2024-05-12 13:56:56 -0400

    config

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,28 @@ import SmallCard from '../components/SmallCard';
 import { projectIcons } from '../components/Icons';
 
 import { projects } from '../utils/projectsData';
+import React, { useState } from 'react';
+const Home = () => {
+  const [inputText, setInputText] = useState('');
 
-const Home = () => (
-  <div className="home">
-    <h1>Can I Haz Deployz?</h1>
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return (
-          <SmallCard
-            key={project.id}
-            Icon={Icon}
-            title={project.name}
-            slug={project.slug}
-          />
-        );
-      })}
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    alert('Button clicked with input:', inputText);
+  };
+
+  return (
+    <div className="home">
+      <h1>Can I Haz Deployz?</h1>
+      <div>
+        <input type="text" id="inputText" value={inputText} onChange={handleInputChange} />
+        <button onClick={handleButtonClick}>Invoke Web API</button>
+      </div>
     </div>
-  </div>
+  );
+};
 );
 
 export default Home;
