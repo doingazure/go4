b22327697234343e2abaa6e8d4ebc6f689c7be7d
Bill Wilder <codingoutloud@gmail.com>
2024-05-12 14:20:42 -0400

    hollow out util

--- pages/index.js
+++ pages/index.js
@@ -12,21 +12,20 @@ function MyComponent() {
 
     // invoke REST API
     var url = 'https://gist.githubusercontent.com/codingoutloud/be2edce7c57e8eee771acd4d15dd77e6/raw/ad4b1b9f2259b11f58a6a04c63c1e86bf27efaf2/foo.json'; 
-    url = './HttpTrigger1';
+    // url = './HttpTrigger1';
     // url = './api/HttpTrigger1';
     // url = './ssldays';
     // url = './api/ssldays';
     
-    url = '/HttpTrigger1';
-    url = '/api/HttpTrigger1';
-    url = '/ssldays';
+    // url = '/HttpTrigger1';
+    // url = '/api/HttpTrigger1';
+    // url = '/ssldays';
     // url = '/api/ssldays';
     
     fetch(url)
       .then((response) => response.json())
       .then((json) => console.log(json));
   };
-
 
   return (
     <div>
@@ -41,22 +40,7 @@ const Home = () => (
     <h1>Deployzing haz Static Appz!</h1>
     <p>Choose website to check:</p>
     <MyComponent />
-
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return (
-          <SmallCard
-            key={project.id}
-            Icon={Icon}
-            title={project.name}
-            slug={project.slug}
-          />
-        );
-      })}
-    </div>
   </div>
 );
 
 export default Home;
-
